4bcdf606571e29bec4b2b44fa91e8a2defd65cd2
李露 <lilu@meishubao.com>
2021-12-27 14:22:10 +0800

    加入starter

---
+++ redismq-spring-boot-starter/src/main/java/com/github/voishion/redismq/RedismqSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.github.voishion.redismq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedismqSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedismqSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ redismq-spring-boot-starter/src/test/java/com/github/voishion/redismq/RedismqSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.voishion.redismq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedismqSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
